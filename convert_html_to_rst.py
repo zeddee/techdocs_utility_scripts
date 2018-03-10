567e71623da7f242c2bc98a03490d040616f8fe8
Zed <weizheng.tan@gmail.com>
2018-03-10 20:52:19 +0800

    add html to rst conversion script

---
+++ convert_html_to_rst.py
@@ -0,0 +1,19 @@
+"""
+Requires
+html2rest==0.2.2
+
+Recurses through all directories looking for `htm` files, and writes a `rst` version of the file to the same directory.
+"""
+
+import os
+from html2rest import html2rest
+
+for dirpath, dirnames, filenames in os.walk("."):
+    path=dirpath.split(os.sep)
+    print (len(path)-1) * '---', os.path.basename(dirpath)
+    for file in filenames:
+        current_file = file.split(os.extsep)
+        if current_file[1] == "htm":
+            write_file = open(os.path.join(dirpath,current_file[0]+'.rst'),"w")
+            html2rest(open(os.path.join(dirpath,file)).read(),writer=write_file)
+            write_file.close()
